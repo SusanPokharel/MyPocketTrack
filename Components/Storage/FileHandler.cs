13e3cd854b9f093d27eef3b57533a45155b373de
Susan Pokharel <pokharelj120@gmail.com>
2025-01-16 22:15:17 +0545

    Added the file handler logic

---
+++ Components/Storage/FileHandler.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MyPocketTrack.Components.Storage
+{
+    public class FileHandler<T>
+    {
+        private static readonly string PreferencesFolder = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MyPocketTrack");
+
+        public static string GetFilePath(string fileName)
+        {
+            if (!Directory.Exists(PreferencesFolder))
+            {
+                Directory.CreateDirectory(PreferencesFolder);
+            }
+
+            return Path.Combine(PreferencesFolder, fileName);
+        }
+
+        public static async Task<List<T>> ReadFromFile(string fileName)
+        {
+            string filePath = GetFilePath(fileName);
+
+            if (!File.Exists(filePath))
+            {
+                return new List<T>();
+            }
+
+            var json = await File.ReadAllTextAsync(filePath);
+            return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+        }
+
+        public static async Task SaveToFile(string fileName, List<T> data)
+        {
+            string filePath = GetFilePath(fileName);
+            var json = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+            await File.WriteAllTextAsync(filePath, json);
+        }
+    }
+}
+
--- obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MyPocketTrack.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MyPocketTrack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPocketTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4b43dab258042c638918b91dc6a737ed2dd6f9aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+95658650d0f46a2c785efe1d0b7ab96c7221d467")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPocketTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPocketTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
